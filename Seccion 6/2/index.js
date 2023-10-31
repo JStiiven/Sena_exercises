2288236e20412e4dc08a84cf9a7a76cae07f5f88
Stiven <113300330+JStiiven@users.noreply.github.com>
2023-10-31 08:29:11 -0500

    Add files via upload

---
+++ Seccion 6/2/index.js
@@ -0,0 +1,6 @@
+let numero = parseInt(prompt("Digite un numero"));
+let suma = 0;
+for(let i = 1; i <= numero; i++){
+    suma += i
+    console.log(suma);
+}
---
+++ Seccion 6/3/index.js
@@ -0,0 +1,5 @@
+let multiplicacion;
+for(let i = 1; i <= 50; i++){
+    multiplicacion = i * 10
+    console.log(10 , "x" , i , "=" , multiplicacion);
+}
